489dd506aac20a9e624c8563f2f99c3250ff6a0f
juanencs <juanenricocs@outlook.com>
2025-08-23 00:25:02 -0300

    Novos arquivos

---
+++ Estr_seq/ex1.c
@@ -0,0 +1,28 @@
+/**
+ * @file ex1.c
+ * @author Juan Enrico
+ * @brief João faz economias em dólar, e deseja saber quanto estas economias valem em reais. Leia a quantidade de dólares que ele possui e a cotação atual do dólar (em real). Converta o valor de suas economias para real. Imprimir: o valor das economias em dólar e em real.
+ * @version 0.1
+ * @date 2025-08-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+
+int main() {
+
+    double e_dolar, e_real, cotaçao; 
+
+    printf ("Insira o valor da economia em dólares: ");
+    scanf ("%lf", &e_dolar);
+
+    printf ("Insira o valor da cotação atual do dólar em real: ");
+    scanf ("%lf", &cotaçao);
+
+    e_real = e_dolar * cotaçao;
+
+    printf ("O valor das economias em reais é: R$ %.2lf\n", e_real);
+
+    return 0;
+}
---
+++ Estr_seq/ex2.c
@@ -0,0 +1,33 @@
+/**
+ * @file ex2.c
+ * @author Juan Enrico
+ * @brief Uma pessoa está construindo e resolveu colocar em sua casa uma caixa para servir como reservatório de água. Considerando que a caixa seja retangular, faça um programa que leia as dimensões da caixa (comprimento, altura e largura), calcule e escreva o volume de água que pode ser armazenado.
+ * @version 0.1
+ * @date 2025-08-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+
+int main() {
+
+    double comprimento, largura, altura, volume;
+
+    printf ("== Insira as medidas da caixa d'água ==\n");
+
+    printf ("Comprimento (m): ");
+    scanf ("%lf", &comprimento);
+
+    printf ("Largura (m): ");
+    scanf ("%lf", &largura);
+
+    printf ("Altura (m): ");
+    scanf ("%lf", &altura);
+
+    volume = comprimento * largura * altura;
+
+    printf ("O volume de água que pode ser armazenado é de: %.2lf m³.\n", volume);
+
+    return 0;
+}
